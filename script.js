d7218234765401133e063e2854d9120b3d319c06
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-06-29 00:10:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log("Welcome to PolyDog!");
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Welcome to PolyDog!");
+
+    const button = document.querySelector(".button");
+
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        alert("🐕 Welcome to the PolyDog Pack! Buy links coming soon.");
+    });
+});
