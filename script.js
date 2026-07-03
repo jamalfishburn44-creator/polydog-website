bb0df22c5e2ed6ef75d1650a062f97e0de14b607
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-07-03 11:16:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,47 @@
 const connectButton = document.getElementById("connectWallet");
 
+window.addEventListener("load", checkConnection);
+
 connectButton.addEventListener("click", connectWallet);
+
+async function checkConnection() {
+    if (!window.ethereum) return;
+
+    try {
+        const accounts = await ethereum.request({
+            method: "eth_accounts"
+        });
+
+        if (accounts.length > 0) {
+            updateButton(accounts[0]);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
 
 async function connectWallet() {
 
-if (typeof window.ethereum === "undefined") {
-    alert("Please install MetaMask to connect your wallet.");
-    window.open("https://metamask.io/download/", "_blank");
-    return;
+    if (!window.ethereum) {
+        alert("Please install MetaMask to connect your wallet.");
+        window.open("https://metamask.io/download/", "_blank");
+        return;
+    }
+
+    try {
+        const accounts = await ethereum.request({
+            method: "eth_requestAccounts"
+        });
+
+        updateButton(accounts[0]);
+
+    } catch (err) {
+        console.error(err);
+        alert("Wallet connection cancelled.");
+    }
 }
 
-try {
-
-const accounts = await window.ethereum.request({
-    method: "eth_requestAccounts"
-});
-
-const account = accounts[0];
-
-connectButton.innerText =
-account.substring(0,6) + "..." +
-account.substring(account.length-4);
-
-} catch (err) {
-
-console.error(err);
-
-alert("Wallet connection cancelled.");
-
-}
-
+function updateButton(account) {
+    connectButton.innerText =
+        account.slice(0, 6) + "..." + account.slice(-4);
 }
