bc42eb2aeb187330f2058ecd6ea896a1f3e60427
jamalfishburn44-creator <jamalfishburn44@gmail.com>
2026-07-04 17:33:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,31 @@
 const connectButton = document.getElementById("connectWallet");
 
-const contractAddress =
-"0xca3b716582b4790c9fdd2672425e82c183b8e3d5";
-
-// Connect Wallet
-if (connectButton) {
-connectButton.addEventListener("click", connectWallet);
-}
-
 async function connectWallet() {
 
-if (!window.ethereum) {
-alert("Please install MetaMask.");
-return;
-}
+    if (!window.ethereum) {
+        alert("MetaMask is not installed.");
+        return;
+    }
 
-try {
+    try {
 
-const accounts = await window.ethereum.request({
-method: "eth_requestAccounts"
-});
+        const accounts = await ethereum.request({
+            method: "eth_requestAccounts"
+        });
 
-const account = accounts[0];
+        const account = accounts[0];
 
-connectButton.innerHTML =
-account.substring(0,6) +
-"..." +
-account.substring(account.length-4);
+        connectButton.innerHTML =
+            account.substring(0,6) +
+            "..." +
+            account.substring(account.length-4);
 
-}
-catch(err){
+    } catch(err) {
 
-console.log(err);
+        console.log(err);
 
-}
-
-}
-
-// Copy Contract
-function copyContract(){
-
-navigator.clipboard.writeText(contractAddress);
-
-const btn =
-document.getElementById("copyButton");
-
-btn.innerHTML="✅ Copied!";
-
-setTimeout(function(){
-
-btn.innerHTML="📋 Copy Contract";
-
-},2000);
+    }
 
 }
+
+connectButton.addEventListener("click", connectWallet);
